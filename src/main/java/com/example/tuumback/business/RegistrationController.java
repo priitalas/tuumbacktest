80e01250d61a5df21cc596b03f499d3127f841ec
Priit <priitalas12@gmail.com>
2024-04-30 16:03:46 +0300

    New customer account WIP

--- src/main/java/com/example/tuumback/business/RegistrationController.java
+++ src/main/java/com/example/tuumback/business/RegistrationController.java
@@ -1,9 +1,11 @@
 package com.example.tuumback.business;
 
 import com.example.tuumback.business.dto.RegistrationRequest;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.jmx.export.annotation.ManagedOperationParameters;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,17 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
-    @GetMapping("/registration")
+    @PostMapping("/registration")
+    // registreerib uue kasutaja
     public void registerNewCustomer(@RequestBody RegistrationRequest registrationRequest) {
         registrationService.registerNewCustomer(registrationRequest);
 
     }
+
+    @GetMapping(/registration)
+    // toob alt uue kasutaja ID ja konto ID ja kontojäägi
+
+
+
 }
+
